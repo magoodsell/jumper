ebed72ddd5f89e2c0b7fc3db0b96d25f6740397c
Marvil Garcia <marvigarcia015@gmail.com>
2022-02-03 22:59:29 -0400

    Update

--- game/word.py
+++ game/word.py
@@ -1,3 +1,14 @@
+import random
 class Word:
     def __init__(self):
-        pass
+
+        
+        words = open('words.txt')
+        self._get_word = random.choice(words)
+        dashboard = [' '] * len(words)
+
+        return self._get_word, dashboard
+
+
+
+    
