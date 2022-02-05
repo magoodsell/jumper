094d10c85993c441d1e3b64ef549d4c0969f6ed1
Cryptum45 <bir19019@byui.edu>
2022-02-04 17:40:27 -0700

    Made the print for the guy

--- game/terminal_service.py
+++ game/terminal_service.py
@@ -1,5 +1,65 @@
+import guess
+import word
 class terminal:
 
-    def __init__(self) -> None:
-        pass
-#Hello
+    def __init__(self):
+        self.guess= guess
+
+   
+   
+    def results(self):
+        if guess != self.guess.getletter():
+            if failedGuesses == 1:
+                print(" --- ")
+                print("/   \ ")
+                print("------")
+                print("\    /")
+                print(" \  / ")
+                print ("  O  " )
+                print("/  |  \ ")
+                print("  / \ " )
+                print("“^^^^^^^^")
+            elif failedGuesses == 2:  
+                print("")
+                print("/   \ ")
+                print("------")
+                print("\    /")
+                print(" \  / ")
+                print ("  O  " )
+                print("/  |  \ ")
+                print("  / \ " )
+                print("“^^^^^^^^")
+            elif failedGuesses == 3:
+                
+                print("")
+                print("")
+                print("------")
+                print("\    /")
+                print(" \  / ")
+                print ("  O  " )
+                print("/  |  \ ")
+                print("  / \ " )
+                print("“^^^^^^^^")
+            elif failedGuesses == 4:
+                print("")
+                print("")
+                print("")
+                print("")
+                print(" \  / ")
+                print ("  O  " )
+                print("/  |  \ ")
+                print("  / \ " )
+                print("“^^^^^^^^")
+            elif failedGuesses == 5:
+                print("")
+                print("")
+                print("")
+                print("")
+                print("")
+                print ("  X  " )
+                print("/  |  \ ")
+                print("  / \ " )
+                print("“^^^^^^^^")
+
+
+	gameOver = True
