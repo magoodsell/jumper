61defe0782a280f26ed118f0093a8eb4cc15f908
magoodsell <goo15004@byui.edu>
2022-02-07 18:06:10 -0700

    Added some changes to guess to count the number of wrong guesses.

--- game/guess.py
+++ game/guess.py
@@ -1,13 +1,24 @@
 import word
 
-
+####
+# To Do:
+# Figure out how to create a method to set the wrong guesses
+#   - getter and a setter.
+#   - do I need a setter if I set it in the check guess class? 
+####
 
 class Guess:
-    """
-    The responsibility of the guess is to take the user guess and update the word. It will pass the
-    word to the terminal and keep track of the number of wrong guesses. 
+    """ A guess from the player.
+    The responsibility of the guess is to:
+        - take the user guess and update the word displayed to the player. 
+        - Keep track of wrong guesses made
+        - Inform the player of duplicate guess 
 
-    Attributes: 
+    Attributes:
+        self._guess (string): an empty string
+        self._word (Word): the random word
+        self._all_guesses (list): a list of all guess made.
+        self._wrong_guesses (int): count of all wrong guesses made.
 
     """
 
@@ -22,6 +33,8 @@ class Guess:
         self._guess = ''
         self._word = word.get_word() # it should probably be _set_word will have to add this to the game.
         self._all_guesses = []
+        self._wrong_guesses = 0
+        self._tick = 0
 
         pass 
 
@@ -30,6 +43,16 @@ class Guess:
         Purpose is to get the word the person has guessed and append to _all_guesses
         """
         return self._all_guesses.append(self._guess)
+
+
+    def get_wrong_guesses(self):
+
+        return self._wrong_guesses
+
+    def set_wrong_guesses(self):
+
+        self._wrong_guesses += self._tick
+        return self._wrong_guesses
 
     def check_duplicate(self, guess):
         """purpose is to make sure the player is not guessing the same letter
@@ -45,13 +68,18 @@ class Guess:
         Checks the guess to make sure it matches a letter and returns an updated word.
         """
         new_word = []
+        self._tick = 0
+        self._wrong_guesses
         for i in len(self._word):
             if i == self._guess:
                 new_word.append(i)
             else: 
                 new_word.append('_')
+                # self._wrong_guesses + 1
+                self._tick += 1
 
-        return new_word 
+
+        return new_word, self._wrong_guesses
         
 
     
