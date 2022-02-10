fa808fa4544830aba83851d607513de84b2eae50
Marvil Garcia <marvigarcia015@gmail.com>
2022-02-09 21:19:51 -0400

    showing letters changes

--- game/word.py
+++ game/word.py
@@ -10,7 +10,7 @@ class Word:
             self (Word): An instance of Word.
         """
         _words = open('words.txt')
-        show_letters = None
+        self.show_letters = None
         self._words = random.choice(_words)
 
 
@@ -18,12 +18,12 @@ class Word:
 
     def get_words(self):
 
-        """Get a word from the list to guess.
+        """Show the underscore lines with the lengh.
 
         Args:
             self (Word): An instance of Word.
         """
-        show_letters = None
+        
         self.show_letters = list("_"*len(self._words))
         return self._words
     
